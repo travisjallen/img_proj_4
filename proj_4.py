c08484cbefc7a205652e793f6c83f605f1046692
travisjallen <t.allen310@gmail.com>
2021-12-01 09:04:34 -0700

    see drawing for procedure

--- proj_4.py
+++ proj_4.py
@@ -4,9 +4,77 @@
 ## Project 4
 #######################
 
+## import necessary libraries
+import numpy as np
+from skimage import io
+import matplotlib.pyplot as plt
+
 ## import json reader
-import read_json
+from read_json import read_json
 
-data = read_json
+## read the json file
+corra,corrb,image_names,corrs = read_json("mosaic_params.json")
 
-## see slide 25 in geometric_trans-1.pdf
+## correct the image names
+for i in range(len(image_names)):
+    temp = image_names[i].split('.')
+    image_names[i]=  temp[0]+'.png'
+
+## read the first image
+img_0 = io.imread(image_names[0])
+img_0 = img_0/np.amax(img_0)
+
+## determine its size
+img_0_size = np.shape(img_0)
+img_0_rows = img_0_size[0]
+img_0_cols = img_0_size[1]
+img_0_layers = img_0_size[2]
+
+## create a canvas
+num_images = len(image_names)
+
+## establish a factor of safety
+fos = num_images + 1
+canvas = np.ndarray((fos*img_0_rows,fos*img_0_cols,img_0_layers))
+canvas[:,:,:] = 0.5
+
+## determine where to place the image
+origin_r = int((fos*img_0_rows/2) - (img_0_rows/2))
+origin_c = int((fos*img_0_cols/2) - (img_0_cols/2))
+terminus_r = int(origin_r + img_0_rows)
+terminus_c = int(origin_c + img_0_cols)
+
+## place it
+canvas[origin_r:terminus_r,origin_c:terminus_c,:] = img_0[:,:,:]
+
+## plot it
+# plt.figure()
+# plt.imshow(canvas,cmap='gray')
+# plt.axis('off')
+# plt.show()
+
+base_image = image_names[0]
+base_identifier = base_image.split('.')
+base_identifier = base_identifier[0]
+print(len(base_identifier))
+
+nconstituents = corrs[0][1][0].split('.')
+nfirst_part = nconstituents[0]
+print(len(nfirst_part))
+
+for i in range(num_images - 1):
+    ## determine if the relative order of the correlations
+    img_name = corrs[i][1][0].split('.')
+    img_identifier = img_name[0]
+    
+    ## check to see if the image has 3 characters before the dot
+    if (len(img_identifier) == 3):
+        ## if it does, pull the last one off
+        img_identifier = img_identifier.rstrip(img_identifier[-1])
+
+    ## now check to see if the base image is the second image in the correlation
+    if (base_identifier == img_identifier):
+        ## if so, swap the columns of the correlation
+        temp = corra[:,:,i]
+        corra[:,:,i] = corrb[:,:,i]
+        corrb[:,:,i] = temp
--- read_json.py
+++ read_json.py
@@ -3,31 +3,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io
 
-with open("mosaic_params.json") as f:
-    data = json.load(f)
+def read_json(filename):
+    with open("mosaic_params.json") as f:
+        data = json.load(f)
 
-print(f"The images are {data['Input files']}")
-corrs = data["Correspondences"]
-print(f"There are {len(corrs)} sets of correspondences")
-for i in range(len(corrs)):
-    print(f"There are {len(corrs[i][0][1])} correspdondences between image {corrs[i][0][0]} and image {corrs[i][1][0]}")
-    corra = np.asarray(corrs[i][0][1])
-    corrb = np.asarray(corrs[i][1][1])   
-    print(corra.T)
-    print(corrb.T)
-    imageNamea = corrs[i][0][0]
-    imageNameb = corrs[i][1][0]
-    imageNamea = imageNamea.split('.')
-    imageNameb = imageNameb.split('.')
-    imagea = io.imread(imageNamea[0]+'.png')
-    imageb = io.imread(imageNameb[0]+'.png')
-    fig, ax = plt.subplots(1,2)
-    fig.set_size_inches(10.5,10.5)
-    ax[0].imshow(imagea)
-    ax[0].scatter(corra[:,0],corra[:,1],c='#ff7f0e')
-    ax[1].imshow(imageb)
-    ax[1].scatter(corrb[:,0],corrb[:,1],c='#ff7f0e')
-    plt.savefig(str(i)+'.png')
-    print(corra.shape)
-    print(corrb.shape)
-print(f"The output file is {data['Output file']}")
+    # print(f"The images are {data['Input files']}")
+    corrs = data["Correspondences"]
+    # print(f"There are {len(corrs)} sets of correspondences")
+
+    ## make arrays to store corra and corrb
+    num_corrs = np.zeros((1,len(corrs)))
+    for i in range(len(corrs)):
+        num_corrs[0,i] = len(corrs[i][0][1])
+    
+    corra = np.ndarray((int(np.amax(num_corrs)),2,len(corrs)))
+    corrb = np.ndarray((int(np.amax(num_corrs)),2,len(corrs)))
+
+
+    for i in range(len(corrs)):
+        # print(f"There are {len(corrs[i][0][1])} correspdondences between image {corrs[i][0][0]} and image {corrs[i][1][0]}")
+        corra[0:int(num_corrs[0,i]),:,i] = np.asarray(corrs[i][0][1])
+        corrb[0:int(num_corrs[0,i]),:,i] = np.asarray(corrs[i][1][1])   
+        # print(corra[:,:,i])
+        # print(corrb.T)
+        imageNamea = corrs[i][0][0]
+        imageNameb = corrs[i][1][0]
+        imageNamea = imageNamea.split('.')
+        imageNameb = imageNameb.split('.')
+        imagea = io.imread(imageNamea[0]+'.png')
+        imageb = io.imread(imageNameb[0]+'.png')
+        fig, ax = plt.subplots(1,2)
+        fig.set_size_inches(10.5,10.5)
+        ax[0].imshow(imagea)
+        ax[0].scatter(corra[:,0,i],corra[:,1,i],c='#ff7f0e')
+        ax[1].imshow(imageb)
+        ax[1].scatter(corrb[:,0,i],corrb[:,1,i],c='#ff7f0e')
+        plt.savefig(str(i)+'.png')
+    #     print(corra.shape)
+    #     print(corrb.shape)
+    # print(f"The output file is {data['Output file']}")
+
+    ## return the correlations and the image names
+    image_names = data['Input files']
+
+    return corra,corrb,image_names,corrs
